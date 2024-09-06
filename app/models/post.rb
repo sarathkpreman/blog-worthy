5e7fb320a41389ba024720a520966d448f0bc112
Sarath K P <ssarathckm@gmail.com>
2024-09-06 18:51:38 +0530

    Added git hooks, eslint, prettier and rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125 unless defined?(MAX_TITLE_LENGTH)
 
@@ -12,25 +14,25 @@ class Post < ApplicationRecord
   validate :check_downvotes_greater_or_not, :check_votes_negative_or_not, :check_netvotes_negative_or_not
 
   private
-  def netvote
-    upvotes - downvotes
-  end
 
-  def check_votes_negative_or_not
-    self.upvotes = [upvotes, 0].max
-    self.downvotes = [downvotes, 0].max
-  end
-
-  def check_netvotes_negative_or_not
-    if netvote < 0
-      errors.add(:netvotes, "cannot be negative")
+    def netvote
+      upvotes - downvotes
     end
-  end
 
-  def check_downvotes_greater_or_not
-    if downvotes > upvotes
-      errors.add(:downvotes, "cannot be more than upvotes")
+    def check_votes_negative_or_not
+      self.upvotes = [upvotes, 0].max
+      self.downvotes = [downvotes, 0].max
     end
-  end
-  
+
+    def check_netvotes_negative_or_not
+      if netvote < 0
+        errors.add(:netvotes, "cannot be negative")
+      end
+    end
+
+    def check_downvotes_greater_or_not
+      if downvotes > upvotes
+        errors.add(:downvotes, "cannot be more than upvotes")
+      end
+    end
 end
