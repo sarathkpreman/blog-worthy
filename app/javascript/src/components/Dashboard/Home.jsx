f7c09025791b509d0601a4525bbbabb66116eb30
Sarath K P <ssarathckm@gmail.com>
2024-09-11 17:30:46 +0530

    floating UI were missing Installed and corrected few errors

--- app/javascript/src/components/Dashboard/Home.jsx
+++ app/javascript/src/components/Dashboard/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import PostsList from "../ActivityStream/PostList";
-import NavBar from "../NavBar/Index";
+import Navbar from "../NavBar/Index";
 
 const Home = () => (
   <div className="flex h-screen flex-col">
     <div className="fixed left-0 right-0 top-0 z-10">
-      <NavBar />
+      <Navbar />
     </div>
     <div className="mx-4 mt-20 flex flex-grow flex-col">
       <div className="flex flex-grow items-center justify-center p-5">
--- app/javascript/src/components/commons/Container.jsx
+++ app/javascript/src/components/commons/Container.jsx
@@ -3,15 +3,10 @@ import React from "react";
 import classnames from "classnames";
 import PropTypes from "prop-types";
 
-import NavBar from "components/NavBar";
-
 const Container = ({ children, className = "" }) => (
-  <>
-    <NavBar />
-    <div className={classnames("mx-auto max-w-6xl px-6", [className])}>
-      {children}
-    </div>
-  </>
+  <div className={classnames("mx-auto max-w-6xl px-6", [className])}>
+    {children}
+  </div>
 );
 
 Container.propTypes = {
